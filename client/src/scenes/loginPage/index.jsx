15cc30dfe16c6666099058ea6c9ae24837e35f86
Shawn <realshawnliu@gmail.com>
2023-05-31 16:40:46 +1000

    fix typo

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -4,7 +4,6 @@ import Form from "./Form";
 const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
   return (
     <Box>
       <Box
@@ -17,6 +16,7 @@ const LoginPage = () => {
           Sociopedia
         </Typography>
       </Box>
+
       <Box
         width={isNonMobileScreens ? "50%" : "93%"}
         p="2rem"
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Sociopedia!
+          Welcome to Socipedia, the Social Media for Sociopaths!
         </Typography>
         <Form />
       </Box>
