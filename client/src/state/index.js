90359b1510f4ff6238e251e263fc2e08912f7064
Shawn <realshawnliu@gmail.com>
2023-05-31 16:50:12 +1000

    fix state typo

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -14,34 +14,35 @@ export const authSlice = createSlice({
     setMode: (state) => {
       state.mode = state.mode === "light" ? "dark" : "light";
     },
-  },
-  setLogin: (state, action) => {
-    state.user = action.payload.user;
-    state.token = action.payload.token;
-  },
-  setLogout: (state) => {
-    state.user = null;
-    state.token = null;
-  },
-  setFriends: (state, action) => {
-    if (state.user) {
-      state.user.friends = action.payload.friends;
-    } else {
-      console.error("User object non-existent");
-    }
-  },
-  setPosts: (state, action) => {
-    state.posts = action.payload.posts;
-  },
-  updatePost: (state, action) => {
-    const updatedPosts = state.posts.map((post) => {
-      if (post._id === action.payload.post._id) {
-        return action.payload.post;
+
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
       } else {
-        return post;
+        console.error("User object non-existent");
       }
-    });
-    state.posts = updatedPosts;
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    updatePost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) {
+          return action.payload.post;
+        } else {
+          return post;
+        }
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
 
