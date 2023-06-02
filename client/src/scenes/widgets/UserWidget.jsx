5e38826d5df41482e6dd8e1aa0b3442c4a755a39
Shawn <realshawnliu@gmail.com>
2023-06-02 15:01:28 +1000

    fix typos

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -19,7 +19,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const token = useSelector((state) => state.token);
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
-  const main = palette.primary.main;
+  const main = palette.neutral.main;
 
   const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
@@ -38,19 +38,31 @@ const UserWidget = ({ userId, picturePath }) => {
     return null;
   }
 
-  const { firstName, lastName, location, occupation, viewedProfile, impressions, friends } = user;
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
 
   return (
     <WidgetWrapper>
-      {/* first row */}
-      <FlexBetween gap="o.5rem" pb="1.1rem" onClick={() => navigate(`/profile/${userId}`)}>
+      {/* FIRST ROW */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
         <FlexBetween gap="1rem">
           <UserImage image={picturePath} />
           <Box>
             <Typography
               variant="h4"
-              fontWeight="500"
               color={dark}
+              fontWeight="500"
               sx={{
                 "&:hover": {
                   color: palette.primary.light,
@@ -67,7 +79,8 @@ const UserWidget = ({ userId, picturePath }) => {
       </FlexBetween>
 
       <Divider />
-      {/* second row */}
+
+      {/* SECOND ROW */}
       <Box p="1rem 0">
         <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
           <LocationOnOutlined fontSize="large" sx={{ color: main }} />
@@ -78,26 +91,33 @@ const UserWidget = ({ userId, picturePath }) => {
           <Typography color={medium}>{occupation}</Typography>
         </Box>
       </Box>
-      {/* third row */}
+
+      <Divider />
+
+      {/* THIRD ROW */}
       <Box p="1rem 0">
-        <flexBetween mb="0.5rem">
-          <Typography color={medium}>Profile views</Typography>
+        <FlexBetween mb="0.5rem">
+          <Typography color={medium}>Who's viewed your profile</Typography>
           <Typography color={main} fontWeight="500">
             {viewedProfile}
           </Typography>
-        </flexBetween>
+        </FlexBetween>
         <FlexBetween>
-          <Typography color={medium}>Impressions</Typography>
+          <Typography color={medium}>Impressions of your post</Typography>
           <Typography color={main} fontWeight="500">
             {impressions}
           </Typography>
         </FlexBetween>
       </Box>
-      {/* fourth row */}
+
+      <Divider />
+
+      {/* FOURTH ROW */}
       <Box p="1rem 0">
         <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
           Social Profiles
         </Typography>
+
         <FlexBetween gap="1rem" mb="0.5rem">
           <FlexBetween gap="1rem">
             <img src="../assets/twitter.png" alt="twitter" />
@@ -116,7 +136,7 @@ const UserWidget = ({ userId, picturePath }) => {
             <img src="../assets/linkedin.png" alt="linkedin" />
             <Box>
               <Typography color={main} fontWeight="500">
-                LinkedIn
+                Linkedin
               </Typography>
               <Typography color={medium}>Network Platform</Typography>
             </Box>
