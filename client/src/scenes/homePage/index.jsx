fb24d89ab60d1df2cef8f4bfa269dc7ccde9c04a
Shawn <realshawnliu@gmai.com>
2023-06-03 16:50:41 +1000

    typos

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,13 @@
 import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-import { useSelector } from "react-redux";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery("min-width:1000px");
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
 
   return (
@@ -26,8 +28,15 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
+          {/* <PostsWidget userId={_id} /> */}
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            {/* <AdvertWidget /> */}
+            <Box m="2rem 0" />
+            {/* <FriendListWidget userId={_id} /> */}
+          </Box>
+        )}
       </Box>
     </Box>
   );
