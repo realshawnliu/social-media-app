2fe6b05240fb9c414d0f2eda6d504389320db657
Shawn <realshawnliu@gmail.com>
2023-06-04 16:36:58 +1000

    fix typos

--- client/src/scenes/widgets/PostFeedWidget.jsx
+++ client/src/scenes/widgets/PostFeedWidget.jsx
@@ -3,27 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
 
-const PostWidget = ({ userId, isUserProfile = false }) => {
+const PostsFeedWidget = ({ userId, isUserProfile = false }) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch(`http://localhost:3001/posts`, {
-      method: "get",
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
 
   const getUserPosts = async () => {
     const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
-      method: "get",
+      method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
@@ -35,38 +33,38 @@ const PostWidget = ({ userId, isUserProfile = false }) => {
       getPosts();
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
 };
 
-return (
-  <>
-    {posts.map(
-      ({
-        _id,
-        userId,
-        firstName,
-        lastName,
-        description,
-        location,
-        picturePath,
-        userPicturePath,
-        likes,
-        comments,
-      }) => {
-        <PostWidget
-          key={_id}
-          postId={_id}
-          postUserId={userId}
-          name={`${firstName} ${lastName}`}
-          description={description}
-          location={location}
-          picturePath={picturePath}
-          userPicturePath={userPicturePath}
-          likes={likes}
-          comments={comments}
-        ></PostWidget>;
-      }
-    )}
-  </>
-);
-
-export default PostWidget;
+export default PostsFeedWidget;
